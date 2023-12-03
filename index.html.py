d3ea02f07b50b26376076e6622d01a917f866bd3
Your Name <you@example.com>
2023-12-03 22:55:06 +0500

    wedfg

---
+++ index.html.py
@@ -0,0 +1,31 @@
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+#   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+#     <link rel="stylesheet" href="./three.jpg">
+#     <link rel="stylesheet" href="style.css">
+#     <title>cv(my cv)</title>
+# </head>
+# <body>
+#     <div class="bibi">
+#         <h2 class="logo">lalalAlex</h2>
+#         <i class="fa-brands fa-discord"></i>
+#         <i class="fa-brands fa-instagram"></i>
+#         <i class="fa-brands fa-telegram"></i>
+#     </div>
+#
+#     <div class="cont">
+#         <img src="./myphoto.jpg" alt="">
+#         <p>Имя: smth i dunno <br>
+#             1. smth <br>
+#             2. smth <br>
+#             <br>
+#             Изучаю python не так давно, но уже знаю framework'и как:
+#             django, bootstrap
+#         </p>
+#     </div>
+# </body>
+# </html>
